c5644182a0fb1898e55c456e37a8fe8d4afc04b0
Ben Myers <ben.myers@okstate.edu>
2022-03-03 17:08:52 -0600

    Fix JSDoc typo

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 /**
  * @typedef {import('@11ty/eleventy/src/UserConfig')} EleventyConfig
  * @typedef {ReturnType<import('@11ty/eleventy/src/defaultConfig')>} EleventyReturnValue
- * @type {(eleventyConfig: EleventyConfig)) => EleventyReturnValue}
+ * @type {(eleventyConfig: EleventyConfig) => EleventyReturnValue}
  */
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/css');
